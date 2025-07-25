65897909e34e1410da6c24f68fbb8b655d942747
Dayer <93074807+dayercher005@users.noreply.github.com>
2025-07-25 22:18:17 +0800

    Added Binary Tree and Node files

---
+++ BinarySearchTree.js
@@ -0,0 +1,57 @@
+import {Node} from "./Node.js";
+
+class Tree {
+
+    constructor(){
+        this.root
+    }
+
+    buildTree(array){
+
+    }
+
+    insert(value){
+
+    }
+
+    deleteItem(value){
+
+    }
+
+    find(value){
+
+    }
+
+    levelOrderForEach(callback){
+
+    }
+
+    inOrderForEach(callback){
+
+    }
+
+    preOrderForEach(callback){
+
+    }
+
+    postOrderForEach(callback){
+        
+    }
+
+    height(value){
+
+    }
+
+    depth(value){
+
+    }
+
+    isBalanced(){
+
+    }
+
+    rebalance(){
+
+    }
+
+}
---
+++ Node.js
@@ -0,0 +1,10 @@
+export {Node};
+
+class Node {
+
+    constructor(data, right, left){
+        this.data = data;
+        this.right = right;
+        this.left = left;
+    }
+}
